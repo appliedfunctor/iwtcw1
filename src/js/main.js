47de8c33621529c9c3dff2109d2405ff0962509a
Alexander Worton <kesomir@btinternet.com>
2017-03-04 12:11:48 +0000

    added javascriptdocs

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+/**
+ * retriee XML content from a supplied url with a callback
+ * @param {*} myUrl the url to request the xml data from
+ * @param {*} callback the function to execute on success
+ */
 function getXML(myUrl, callback) {
   $.ajax({
     url: myUrl,
@@ -5,14 +10,19 @@ function getXML(myUrl, callback) {
     success: function(data){
       callback(data);
     },
-    error: function(xhr, status, errorThrown){
+    error: function(xhr, status, error){
       console.log('ajax call failed');
-      console.log( "Error: " + errorThrown );
+      console.log( "Error: " + error );
       console.log( "Status: " + status );
     }
   });  
 }
 
+/**
+ * start up function after the dom has finished loading
+ * grabs the xml and style documents in turn and then passes
+ * them for processing
+ */
 $(document).ready(function() {
   getXML('remakes.xml', function(xml) {
     getXML('table-style.xsl', function(style){
@@ -24,10 +34,11 @@ $(document).ready(function() {
   
 });
 
-function getStylesheet(){
-
-}
-
+/**
+ * Logic function to handle case where browser support is missing for XSLT Processing
+ * @param {*} style the url to the xsl stylesheet
+ * @param {*} xml the url to the xml document
+ */
 function populateXSL(style, xml){
   if (typeof (XSLTProcessor) != "undefined") {
     processXsl(style, xml);
@@ -37,6 +48,11 @@ function populateXSL(style, xml){
   }
 }
 
+/**
+ * function to process the xml with the stylesheet
+ * @param {*} style the url to the xsl stylesheet
+ * @param {*} xml the url to the xml document
+ */
 function processXsl(style, xml){
   var processor = new XSLTProcessor();
   processor.importStylesheet(style);
